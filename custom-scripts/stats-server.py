830d7cb48e03d3a9cd259cc05422cbfed4731ca1
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-09-06 21:58:09 +0000

    add source files for system info

--- custom-scripts/stats-server.py
+++ custom-scripts/stats-server.py
@@ -30,33 +30,44 @@ class MyHandler(BaseHTTPRequestHandler):
 
 
 def get_system_datetime() -> str:
+    #/proc/driver/rtc
     return ""
 
 def get_system_uptime_seconds() -> str:
+    # /proc/uptime
     return ""
 
 def get_processor_model_and_velocity() -> str:
+    # /proc/cpuinfo
     return ""
 
 def get_percentage_processor_in_use() -> float:
+    # /proc/stat
     return 0.0
 
 def get_total_and_used_ram() -> str:
+    # /proc/meminfo
     return ""
 
 def get_system_version() -> str:
+    # /proc/version
     return ""
 
 def get_processes_in_execution() -> list:
+    # ler os subdiretorios de /proc e capturar apenas aqueles com valores numricos (expressao regular ^[0-9]+$)
     return ""
 
 def get_disk_units_with_capacity() -> list:
+    # /proc/partitions (para listar as unidades de disco)
+    # sys/block/[device]/size (para o tamamnho em blocos - 1 bloco = 512 bytes)
     return ""
 
 def get_usb_devices_with_port() -> list:
+    # /sys/bus/usb/devices
     return ""
 
 def get_network_adapters_with_ip() -> list:
+    #/proc/net/route
     return ""
 
 if __name__ == '__main__':
