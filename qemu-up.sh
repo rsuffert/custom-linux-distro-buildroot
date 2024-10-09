4b07aaf8d2a1d2f334ffdd4027d774b13fbf4ef3
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-10-09 21:11:50 +0000

    Tutorial 2.4: Noções sobre IO-Scheduler e acesso a disco (v1.0)

--- qemu-up.sh
+++ qemu-up.sh
@@ -4,4 +4,8 @@ qemu-system-i386 --device e1000,netdev=eth0,mac=aa:bb:cc:dd:ee:ff \
 	--netdev tap,id=eth0,script=custom-scripts/qemu-ifup \
 	--kernel output/images/bzImage \
 	--hda output/images/rootfs.ext2 --nographic \
+	--hdb sdb.bin \
+	--nographic \
 	--append "console=ttyS0 root=/dev/sda"
+
+# gerar arquivo sbd.bin com dd if=/dev/zero of=sdb.bin bs=512 count=2097152 (1GB)
